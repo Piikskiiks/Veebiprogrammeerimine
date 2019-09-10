9d08372908542416d4db1706e9bb936a629ab4e5
Kiikspiiks@users.noreplay.github.com <aleksrob@greeny.cs.tlu.ee>
2019-09-10 11:09:16 +0300

    esimene üleslaadimine

---
+++ 02 Tund/myindex.php
@@ -0,0 +1,20 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Mingi imelik pealkiri</title>
+
+</head>
+<body>
+  <h1>Jehoova tunnistajate seminar Tallinnas</h1>
+  <p>Reedel kell 16.00 toimub Jehoova tunnistajte seminar aadressil Narva maantee 44.
+  See leht on loodud õppimise raames ning ei sisalda tõsist informatsiooni!</p>
+  <p>Kodutööd on teha tore</p><p>Proovin veel siis <br> mingi uue paragrahvi luua</p>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</p>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin<a/>.</p>
+</body>
+</html>
---
+++ 02 Tund/testpage.php
@@ -0,0 +1,21 @@
+<?php
+  $userName = "Aleksandr Rõbakov";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+  Mingi imelik pealkiri</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName .", tunnistajate seminar Tallinnas</h1>";
+  ?>
+  <p>Reedel kell 16.00 toimub Jehoova tunnistajte seminar aadressil Narva maantee 44.See leht on loodud õppimise raames ning ei sisalda tõsist informatsiooni!</p>
+</body>
+</html>
